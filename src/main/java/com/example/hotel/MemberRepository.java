1c44bb0f364a46ef1373b9193a949f04c7629ebc
FREE-M-Y <m.yoshida.apply@gmail.com>
2024-06-07 17:15:25 +0900

    管理側：会員検索機能追加

--- src/main/java/com/example/hotel/MemberRepository.java
+++ src/main/java/com/example/hotel/MemberRepository.java
@@ -1,5 +1,7 @@
 package com.example.hotel;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +10,9 @@ public interface MemberRepository extends JpaRepository<Member, Integer> {
 
     Member findByMemberEmailAndMemberPass(String memberEmail,String memberPass);
     Member findByMemberEmail(String memberEmail);
-    Member findByMemberId(int memberId);
+    List<Member> findByMemberId(int memberId);
     Member findByMemberWithdrawal(String memberWithdrawal);
+    List<Member> findByMemberNameLike(String MemberName);
+    List<Member> findByMemberAddressLike(String MemberAddress);
 
 }
