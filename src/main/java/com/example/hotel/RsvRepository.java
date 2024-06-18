220b5ccf05695e0ab9f4ccbf7840874756f68a13
FREE-M-Y <m.yoshida.apply@gmail.com>
2024-06-18 16:37:29 +0900

    一般ログイン側：会員退会機能・予約一覧/予約変更機能追加

--- src/main/java/com/example/hotel/RsvRepository.java
+++ src/main/java/com/example/hotel/RsvRepository.java
@@ -1,9 +1,12 @@
 package com.example.hotel;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RsvRepository extends JpaRepository<Rsv, Integer> {
-    
+    List<Rsv> findByMemberId(int memberId);
+    Rsv findByRsvId(int rsvId);
 }
